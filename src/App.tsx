cfdcec46f620fe271d1fbb7b3814464d2c275f1b
Nick <romanov_nikita@hotmail.com>
2021-09-29 14:16:38 +0300

    edit dependency

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="app__headpanel"></div>
+      <div className="app__leftpatel"></div>
+      <div className="app__dashboard"></div>
+      <div className="app__footer"></div>
     </div>
   );
 }
